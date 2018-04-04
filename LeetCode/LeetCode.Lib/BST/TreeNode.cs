2f500454b9445729e869125835558e0c6309686b
Lam Le <lam281990@gmail.com>
2018-04-04 00:01:08 -0700

    Add initial projects

---
+++ LeetCode/LeetCode.Lib/BST/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Lib.BST
+{
+    public class TreeNode
+    {
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public int Val { get; }
+
+        public TreeNode(int val)
+        {
+            Val = val;
+        }
+    }
+}
