4c8be61ec2ac4b80e1256b1858720861361fe212
Lam Le <lam281990@gmail.com>
2018-04-04 21:48:43 -0700

    Trapping rain submitted

--- LeetCode/LeetCode.Lib/TrappingRainWater.cs
+++ LeetCode/LeetCode.Lib/TrappingRainWater.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Lib
 {
@@ -50,18 +52,77 @@ namespace LeetCode.Lib
         public static int NewTrap(int[] height)
         {
             if (!height.Any()) return 0;
-            var waterDrops = 0;
-            var highest = 0;
 
-            for (int i = 0; i < height.Length - 2; i++)
+            var i = 0;
+            var processedInput = height.Select(val =>
             {
-                var currentColumn = height[i];
+                var item = (index: i, value: val);
+                i++;
+                return item;
+            }).ToArray();
+            var firstDivider = GetDivider(processedInput, true);
+            var leftPart = DivideAndConquer(processedInput.Take(firstDivider.index + 1), firstDivider, false);
+            var rightPart = DivideAndConquer(processedInput.Skip(firstDivider.index), firstDivider, true);
 
-                if (currentColumn > highest) highest = currentColumn;
 
+
+            int DivideAndConquer(IEnumerable<(int index, int value)> input, (int index, int value) divider, bool goRight)
+            {
+                var inputCount = input.Count();
+                if (inputCount < 3) return 0;
+
+                var pocketInThisRun = 0;
+
+                if (goRight)
+                {
+                    var newDivider = GetDivider(input.Skip(1), goRight);
+
+                    pocketInThisRun = CountDrop(input.Take(newDivider.index - divider.index + 1));
+
+                    var lastItemIndex = input.Last().index;
+
+                    return pocketInThisRun + DivideAndConquer(input.TakeLast(lastItemIndex - newDivider.index + 1),
+                               newDivider, goRight);
+                }
+                else
+                {
+                    var newDivider = GetDivider(input.SkipLast(1), goRight);
+
+                    pocketInThisRun = CountDrop(input.TakeLast(divider.index - newDivider.index + 1));
+
+                    var firstItemIndex = input.First().index;
+                    return pocketInThisRun + DivideAndConquer(input.Take(newDivider.index - firstItemIndex + 1),
+                               newDivider, goRight);
+                }
             }
 
-            return waterDrops;
+            (int index, int value) GetDivider(IEnumerable<(int index, int value)> input, bool goRight)
+            {
+                if (goRight)
+                {
+                    //We want the get the highest value that is furthest to the right
+                    return input.Aggregate((previousValue, currentValue) =>
+                        (previousValue.value > currentValue.value) ? previousValue : currentValue);
+                }
+
+                return input.Aggregate((previousValue, currentValue) =>
+                    (currentValue.value > previousValue.value) ? currentValue : previousValue);
+            }
+
+            int CountDrop(IEnumerable<(int index, int value)> input)
+            {
+                var count = input.Count();
+                if (count < 3) return 0;
+                var dirtCount = input.Skip(1).Take(count - 2)
+                    .Aggregate(0, (dirt, currentGround) => dirt + currentGround.value);
+                var first = input.First();
+                var last = input.Last();
+                var x = last.index - first.index - 1;
+                var y = Math.Min(first.value, last.value);
+                return x * y - dirtCount;
+            }
+
+            return leftPart + rightPart;
         }
     }
 }
--- LeetCode/LeetCode.Test/TrappingRainWaterTest.cs
+++ LeetCode/LeetCode.Test/TrappingRainWaterTest.cs
@@ -12,15 +12,16 @@ namespace LeetCode.Test
                 {Enumerable.Empty<int>().ToArray(),0 },
                 {new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6 },
                 {new[] { 2, 1, 0, 2 }, 3 },
-                {new []{ 5, 2, 1, 2, 1, 5 ,1}, 14 },
-                {new []{ 10527, 740, 9013, 1300, 29680, 4898, 13993, 15213, 18182, 24254, 3966, 24378, 11522, 9190, 6389, 32067, 21464, 7115, 7760, 3925 } ,2222}
+                {new[] { 5, 2, 1, 2, 1, 5 ,1}, 14 },
+                {new[] { 2 ,0, 2}, 2},
+                {new[] { 10527, 740, 9013, 1300, 29680, 4898, 13993, 15213, 18182, 24254, 3966, 24378, 11522, 9190, 6389, 32067, 21464, 7115, 7760, 3925 } ,2222}
             };
 
         [Theory]
         [MemberData(nameof(TestData))]
         public void Test(int[] terrain, int expectedDrops)
         {
-            var drops = TrappingRainWater.Trap(terrain);
+            var drops = TrappingRainWater.NewTrap(terrain);
             Assert.Equal(expectedDrops, drops);
         }
     }
