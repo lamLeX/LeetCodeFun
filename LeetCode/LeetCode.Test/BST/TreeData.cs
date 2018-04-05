bed74bfdc1404bb27f8f2a546a4db899fdf63f75
Lam Le <lam281990@gmail.com>
2018-04-04 17:34:39 -0700

    Add test data

---
+++ LeetCode/LeetCode.Test/BST/TreeData.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Test.BST
+{
+    public class TreeData
+    {
+        public TreeData()
+        {
+            var arr = new int?[] { 5, 3, 6, 2, 4, null, 7 };
+            var arr2 = new int?[]
+            {
+                2, 0, 33, null, 1, 25, 40, null, null, 11, 31, 34, 45, 10, 18, 29, 32, null, 36, 43, 46, 4, null, 12,
+                24, 26, 30, null, null,
+                35, 39, 42, 44, null, 48, 3, 9, null, 14, 22, null, null, 27, null, null, null, null, 38, null, 41,
+                null, null, null, 47, 49, null, null, 5,
+                null, 13, 15, 21, 23, null, 28, 37, null, null, null, null, null, null, null, null, 8, null, null, null,
+                17, 19, null, null, null, null, null,
+                null, null, 7, null, 16, null, null, 20, 6
+            };
+        }
+    }
+}
