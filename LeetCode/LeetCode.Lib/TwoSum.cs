65e7cc9df71619fcb71bbb2a88c219153dab7516
Lam Le <lam281990@gmail.com>
2018-04-05 08:36:02 -0700

    Two Sum implemented

---
+++ LeetCode/LeetCode.Lib/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Lib
+{
+    public class TwoSum
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var lookup = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (lookup.ContainsKey(complement))
+                {
+                    return new[] { lookup[complement], i };
+                }
+                lookup.Add(nums[i], i);
+            }
+
+            return Enumerable.Empty<int>().ToArray();
+        }
+    }
+}
